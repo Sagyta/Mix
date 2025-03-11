697d19bc2b026a2f023281acc6357269446fa618
Sagyta <betinarenaudo@gmail.com>
2025-03-11 12:14:13 -0300

    tratando de que funcione

--- api/src/app.js
+++ api/src/app.js
@@ -28,6 +28,13 @@ server.use('/', routes);
 //imagenes ads
 server.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 
+// Sirve los archivos estáticos de React desde el directorio build
+server.use(express.static(path.join(__dirname, 'build')));
+
+// Redirige todas las demás rutas a index.html
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Ruta de la API principal (para pruebas, si es necesario)
 server.get('/api/test', (req, res) => {
