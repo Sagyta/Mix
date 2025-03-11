5504ed5a82a0a8101263215246dd5df816e3041f
Sagyta <betinarenaudo@gmail.com>
2025-03-11 00:34:56 -0300

    Cambios host

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ const {buildUser} = require('./src/Preload/index')
 const PORT = process.env.PORT || 3001
 
 conn.sync({ force: false }).then(() => { 
+
+  server.get('/', (req, res) => {
+    res.send('¡Hola Mundo!');
+  });
   
   server.listen(PORT, () => {
     console.log('Servidor corriendo en 3001'); 
