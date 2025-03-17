5f65d28e35a93e2eaafe56bd7fc4231f00e76c9c
Sagyta <betinarenaudo@gmail.com>
2025-03-17 01:21:51 -0300

    agregando y arreglando cosas

--- api/src/app.js
+++ api/src/app.js
@@ -12,11 +12,11 @@ const server = express();
 
 server.name = 'API';
 
-//const frontUrl = process.env.HOST_FRONT;
-const frontApi= process.env.HOST_API;
+const frontUrl = process.env.HOST_FRONT;
+//const frontApi= process.env.HOST_API;
 // Configurar CORS
 server.use(cors({
-    origin: [/*frontUrl,*/ frontApi], 
+    origin: [frontUrl/*, frontApi*/], 
     credentials: true
 }));
 
